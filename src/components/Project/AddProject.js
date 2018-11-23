e84de0e3f707a3d17253065a3d244c387f94790b
dapperauteur <a@awews.com>
2018-11-22 23:36:56 -0500

    add route to Dashboard and AddProject component, refactor App.js to include new routes, add AddProject and CreateProjectButton

---
+++ src/components/Project/AddProject.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AddProject extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Project Form</h1>
+      </div>
+    );
+  }
+}
