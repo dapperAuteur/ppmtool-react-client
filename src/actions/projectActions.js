1ad955078357b6129921eb6f4f2f6ca0cac0a704
dapperauteur <a@awews.com>
2018-11-24 01:07:43 -0500

    add deleteProject to client side

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
@@ -40,4 +40,12 @@ export const getProject = (id, history) => async dispatch => {
   } catch (error) {
     history.push("/dashboard");
   }
+};
+
+export const deleteProject = id => async dispatch => {
+  axios.delete(`http://localhost:8080/api/ver0001/projects/${id}`);
+  dispatch({
+    type: DELETE_PROJECT,
+    payload: id
+  });
 };
