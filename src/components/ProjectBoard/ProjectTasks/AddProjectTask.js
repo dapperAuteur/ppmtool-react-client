55ce84bae3e95924a8d6f98cbb114bdb59bd8f24
dapperauteur <a@awews.com>
2018-11-29 16:42:01 -0500

    add backlogActions

--- src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { addProjectTask } from "./../../../actions/backlogActions";
+import PropTypes from "prop-types";
 
 class AddProjectTask extends Component {
+  constructor(props) {
+    super(props);
+    const { id } = this.props.match.params;
+    this.state = {
+      summary: "",
+      acceptanceCriteria: "",
+      status: "",
+      priority: 0,
+      dueDate: null,
+      projectIdentifier: id
+    };
+  }
   render() {
     const { id } = this.props.match.params;
     return (
@@ -21,6 +37,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     name="summary"
                     placeholder="Project Task summary"
+                    value={this.state.summary}
                   />
                 </div>
                 <div className="form-group">
@@ -28,6 +45,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     placeholder="Acceptance Criteria"
                     name="acceptanceCriteria"
+                    value={this.state.acceptanceCriteria}
                   />
                 </div>
                 <h6>Due Date</h6>
@@ -36,12 +54,14 @@ class AddProjectTask extends Component {
                     type="date"
                     className="form-control form-control-lg"
                     name="dueDate"
+                    value={this.state.dueDate}
                   />
                 </div>
                 <div className="form-group">
                   <select
                     className="form-control form-control-lg"
                     name="priority"
+                    value={this.state.priority}
                   >
                     <option value={0}>Select Priority</option>
                     <option value={1}>High</option>
@@ -54,6 +74,7 @@ class AddProjectTask extends Component {
                   <select
                     className="form-control form-control-lg"
                     name="status"
+                    value={this.state.status}
                   >
                     <option value="">Select Status</option>
                     <option value="TO_DO">TO DO</option>
@@ -74,4 +95,12 @@ class AddProjectTask extends Component {
     );
   }
 }
-export default AddProjectTask;
+
+AddProjectTask.propTypes = {
+  addProjectTask: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { addProjectTask }
+)(AddProjectTask);
