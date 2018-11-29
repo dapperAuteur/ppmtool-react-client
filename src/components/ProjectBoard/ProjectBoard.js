9653abc232dfa12ace5fa0577556cd415d4a04f7
dapperauteur <a@awews.com>
2018-11-29 16:15:09 -0500

    fix route on ProjectBoard, and remove set variable in projectActions

--- src/components/ProjectBoard/ProjectBoard.js
+++ src/components/ProjectBoard/ProjectBoard.js
@@ -7,7 +7,7 @@ class ProjectBoard extends Component {
     const { id } = this.props.match.params;
     return (
       <div className="container">
-        <Link to={`a/addProjectTask/${id}`} className="btn btn-primary mb-3">
+        <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
           <i className="fas fa-plus-circle"> Create Project Task</i>
         </Link>
         <br />
