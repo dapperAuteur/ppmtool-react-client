2afdcb7f50c39c1d28c336fc8506c01b694b4a00
dapperauteur <a@awews.com>
2018-12-01 16:30:49 -0500

    user can login and store token in localstorage

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "./../secureUtils/setJWTToken";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatch => {
   try {
@@ -9,6 +11,32 @@ export const createNewUser = (newUser, history) => async dispatch => {
     dispatch({
       type: GET_ERRORS,
       payload: {}
+    });
+  } catch (err) {
+    console.log(err.response.data);
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
+
+export const login = LoginRequest => async dispatch => {
+  try {
+    //  post => Login Request
+    const res = await axios.post("/users/login", LoginRequest);
+    // extract token from res.data
+    const { token } = res.data;
+    // store the token in the localStorage
+    localStorage.setItem("jwtToken", token);
+    // set our token in header ***
+    setJWTToken(token);
+    // decode token on React
+    const decoded = jwt_decode(token);
+    // dispacth to our securityReducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded
     });
   } catch (err) {
     console.log(err.response.data);
