9d936b544f87de845a72429085d7773f0846b6af
dapperauteur <a@awews.com>
2018-12-01 18:07:05 -0500

    lock public routes when logged in

--- src/components/Layout/Landing.js
+++ src/components/Layout/Landing.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 class Landing extends Component {
+  componentDidMount() {
+    if (this.props.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   render() {
     return (
       <div className="landing">
@@ -31,4 +39,12 @@ class Landing extends Component {
   }
 }
 
-export default Landing;
+Landing.PropTypes = {
+  security: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  security: state.security
+});
+
+export default connect(mapStateToProps)(Landing);
--- src/components/UserManagement/Register.js
+++ src/components/UserManagement/Register.js
@@ -18,6 +18,12 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentDidMount() {
+    if (this.props.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     this.setState({ errors: nextProps.errors });
   }
@@ -122,11 +128,13 @@ class Register extends Component {
 
 Register.propTypes = {
   createNewUser: PropTypes.func.isRequired,
-  errors: PropTypes.object.isRequired
+  errors: PropTypes.object.isRequired,
+  security: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  errors: state.errors
+  errors: state.errors,
+  security: state.security
 });
 
 export default connect(
